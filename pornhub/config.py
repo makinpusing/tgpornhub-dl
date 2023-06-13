c49c47e1214a3a0499c094ec2c96211dad80465f
MakinPusing <90222170+makinpusing@users.noreply.github.com>
2023-06-14 04:06:05 +0700

    Update config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -1,16 +1,14 @@
 from typing import List
 
-API_ID: int = insert_your_own_api_id_here
-API_HASH: str = "insert_your_own_api_hash_here"
-TOKEN: str = "insert_your_bot_token_here"
-
-log_chat: int = insert_log_chat_id_here
-sub_chat: str = "insert_channel_username_here"
+API_ID: int = 27722625
+API_HASH: str = "20f5136ebd6de481c38927d9ccd71fcb"
+TOKEN: str = "6099758412:AAHNJNdJb1K2BI9jps-SJvBXfof2Xl5XbdY"
+log_chat: int = -1001963981088
+sub_chat: str = "introvertmodesleep"
 sudoers: List[int] = [1757169682, 859229457]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
 
 # notes
-
 # 1. api_id & api_hash get from my.telegram.org
 # 2. token fill with your bot_token get from @BotFather
 # 3. log_chat fill with the chat id of a group that you should create for the bot logger
