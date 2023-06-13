ad4da7679940cac73296d2d6ab727561725b0f54
MakinPusing <90222170+makinpusing@users.noreply.github.com>
2023-06-14 04:15:11 +0700

    Update function.py

--- pornhub/plugins/function.py
+++ pornhub/plugins/function.py
@@ -1,5 +1,6 @@
 import asyncio
 import threading
+import time
 from pyrogram.errors import MessageNotModified, FloodWait
 
 
@@ -35,4 +36,5 @@ def download_progress_hook(d, message, client):
         percent = d.get("_percent_str", "N/A")
         speed = d.get("_speed_str", "N/A")
         to_edit = f"📥 <b>Downloading!</b>\n\n<b>Name :</b> <code>{file_name}</code>\n<b>Size :</b> <code>{total}</code>\n<b>Speed :</b> <code>{speed}</code>\n<b>ETA :</b> <code>{eta}</code>\n\n<b>Percentage: </b> <code>{current}</code> from <code>{total} (__{percent}__)</code>"
+        time.sleep(1)
         threading.Thread(target=edit_msg, args=(client, message, to_edit)).start()
